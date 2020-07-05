c7fd192e68782c8601ba7018352a34fc85e63e8c
Mauro Moura <devmauromoura@gmail.com>
2020-07-04 23:25:21 -0300

    fix: ajuste de listagem de nome de usuario, assim como redirecionamento caso ja esteja salvo na memoria

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
     Safe,
     Scroll,
@@ -14,68 +14,73 @@ import {
     ButtonText,
     ButtonIcon
 } from './styles';
+import AsyncStorage from '@react-native-community/async-storage';
 
-const Home = (props) => {
-    const { navigation } = props;
-    React.useLayoutEffect(() => {
-        navigation.setOptions({
-            headerTitle: 'Olá Fulano!',
-            headerLeft: false
-        });
-      }, [navigation]);
+class Home extends Component {
+    async componentDidMount() {
+        const { navigation } = this.props;
+        const name = await AsyncStorage.getItem('@user/name');
+        console.log('SDNAD', name)
+            navigation.setOptions({
+                headerTitle: `Olá ${name ? name : 'usuário'}.`,
+                headerLeft: false
+            });
+    }
+    render() {
+        return (
+            <Safe>
+                <Scroll>
+                    <Container>
+                        <Header>
+                            <LastListContainer>
+                                <LastTitle>Últimas Listas</LastTitle>
+                                <Card>
+                                    <CardText>Jantar de Aniversário</CardText>
+                                    <CardText> - </CardText>
+                                    <CardText>há 5 dias</CardText>
+                                </Card>
+                                <Card>
+                                    <CardText>Jantar de Aniversário</CardText>
+                                    <CardText> - </CardText>
+                                    <CardText>há 5 dias</CardText>
+                                </Card>
+                                <Card>
+                                    <CardText>Jantar de Aniversário</CardText>
+                                    <CardText> - </CardText>
+                                    <CardText>há 5 dias</CardText>
+                                </Card>
+                                <Card>
+                                    <CardText>Jantar de Aniversário</CardText>
+                                    <CardText> - </CardText>
+                                    <CardText>há 5 dias</CardText>
+                                </Card>
+                                <Card>
+                                    <CardText>Jantar de Aniversário</CardText>
+                                    <CardText> - </CardText>
+                                    <CardText>há 5 dias</CardText>
+                                </Card>
+                            </LastListContainer>
+                        </Header>
+                        <Body>
+                            <Button>
+                                <ButtonIcon name="format-list-bulleted" />
+                                <ButtonText>LISTAS</ButtonText>
+                            </Button>
+                            <Button onPress={() => navigation.navigate('Products')}>
+                                <ButtonIcon name="folder-plus" />
+                                <ButtonText>PRODUTOS</ButtonText>
+                            </Button>
+                            <Button>
+                                <ButtonIcon name="cogs" />
+                                <ButtonText>CONF.</ButtonText>
+                            </Button>
+                        </Body>
+                    </Container>
+                </Scroll>
+            </Safe>
+        );
+    }
 
-    return (
-        <Safe>
-            <Scroll>
-                <Container>
-                    <Header>
-                        <LastListContainer>
-                            <LastTitle>Últimas Listas</LastTitle>
-                            <Card>
-                                <CardText>Jantar de Aniversário</CardText>
-                                <CardText> - </CardText>
-                                <CardText>há 5 dias</CardText>
-                            </Card>
-                            <Card>
-                                <CardText>Jantar de Aniversário</CardText>
-                                <CardText> - </CardText>
-                                <CardText>há 5 dias</CardText>
-                            </Card>
-                            <Card>
-                                <CardText>Jantar de Aniversário</CardText>
-                                <CardText> - </CardText>
-                                <CardText>há 5 dias</CardText>
-                            </Card>
-                            <Card>
-                                <CardText>Jantar de Aniversário</CardText>
-                                <CardText> - </CardText>
-                                <CardText>há 5 dias</CardText>
-                            </Card>
-                            <Card>
-                                <CardText>Jantar de Aniversário</CardText>
-                                <CardText> - </CardText>
-                                <CardText>há 5 dias</CardText>
-                            </Card>
-                        </LastListContainer>
-                    </Header>
-                    <Body>
-                        <Button>
-                            <ButtonIcon name="format-list-bulleted"/>
-                            <ButtonText>LISTAS</ButtonText>
-                        </Button>
-                        <Button onPress={() => navigation.navigate('Products')}>
-                            <ButtonIcon name="folder-plus"/>
-                            <ButtonText>PRODUTOS</ButtonText>
-                        </Button>
-                        <Button>
-                            <ButtonIcon name="cogs"/>
-                            <ButtonText>CONF.</ButtonText>
-                        </Button>
-                    </Body>
-                </Container>
-            </Scroll>
-        </Safe>
-    );
 }
 
 export default Home;
--- src/screens/init/index.js
+++ src/screens/init/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, Component } from 'react';
 import {
     Safe,
     Scroll,
@@ -15,18 +15,37 @@ import {
 } from './styles';
 import logo from '../../assets/img/logo.png';
 import * as Animatable from 'react-native-animatable';
+import AsyncStorage from '@react-native-community/async-storage';
 import { Modal } from './components/modalname';
 
-const IndexScreen = (props) => {
-    const [visible, setVisible] = useState(false);
-    const [name, setName] = useState('');
-    const nextpage = () => {
-        const { navigation } = props;
-        if(name.length >= 1){
-            navigation.navigate('Home')
+class IndexScreen extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            visible: false,
+            name: ''
         }
     }
 
+        async componentDidMount(){
+            const { navigation } = this.props;
+            const name = await AsyncStorage.getItem('@user/name');
+
+            if(name){
+                navigation.navigate('Home');
+            }
+        }
+
+     nextpage = async() => {
+        const { navigation } = this.props;
+        const { name } = this.state;
+        if(name.length >= 1){
+            await AsyncStorage.setItem('@user/name', name);
+            navigation.navigate('Home');
+        }
+    }
+    render(){
+        const { name, visible } = this.state;
     return (    
         <Safe>
             <Scroll>
@@ -38,7 +57,7 @@ const IndexScreen = (props) => {
                             <Message>Seja bem vindo(a) ao BuyPlus</Message>
                             <Message>O seu aplicativo para lista de compras.</Message>
                             <ButtonContainer>
-                                <Button onPress={() => setVisible(true)}>
+                                <Button onPress={() => this.setState({ visible: true})}>
                                     <ButtonText>PRONTO</ButtonText>
                                 </Button>
                             </ButtonContainer>
@@ -47,11 +66,12 @@ const IndexScreen = (props) => {
                             <TextVersion></TextVersion>
                         </Footer>
                     </Animatable.View>
-                    <Modal visible={visible} text={name} setText={setName} close={setVisible} save={nextpage}/>
+                    <Modal visible={visible} text={name} setText={val => this.setState({name: val})} close={() => this.setState({visible: false})} save={() => this.nextpage()}/>
                 </Container>
             </Scroll>
         </Safe>
     )
+}
 }
 
 export default IndexScreen;
